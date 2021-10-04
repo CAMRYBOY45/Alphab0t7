6376914046ff5407138fd85243e917a3a8685224
Mr.C1TY3920 <44989007+CAMRYBOY45@users.noreply.github.com>
2021-10-04 15:45:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ apt-get install wget
 apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm i @adiwajshing/baileys@latest 
 npm install
 
 echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
